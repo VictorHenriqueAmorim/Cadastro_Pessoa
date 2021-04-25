7c463a247d3c935bde8810131af57f010f4eaa6c
Vito <victoramorim895623@gmail.com>
2021-04-24 21:23:29 -0300

    Add Entities

---
+++ Models/Entities/DadosCadastro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cadastro_Pessoa.Models.Entities
+{
+    public class DadosCadastro : Entity
+    {
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string CNPJ { get; set; }
+        public string RazaoSocial { get; set; }
+        public string NomeFantasia { get; set; }
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+    }
+}
